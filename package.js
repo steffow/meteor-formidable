66920f3ed6c800580585082e60526e0481dcfac9
Steffo Weber <steffo.weber@gmail.com>
2017-11-04 22:13:17 +0100

    Updated to recent formidable version

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
-  name: 'andruschka:formidable',
-  summary: 'A node.js module for parsing form data, especially file uploads.',
-  version: '1.0.0',
-  git:  "https://github.com/andruschka/meteor-formidable.git"
+  name: 'steffo:formidable',
+  summary: 'A node.js module for parsing form data, especially file uploads. Based on Andruschkas version',
+  version: '1.0.1',
+  git:  "https://github.com/steffow/meteor-formidable.git"
 });
 
 Npm.depends({
-  "formidable":"1.0.15"
+  "formidable":"1.1.1"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0');
+  api.versionsFrom('1.6');
   api.addFiles('include-formidable.js', 'server');
   api.export('formidable','server');
 });
